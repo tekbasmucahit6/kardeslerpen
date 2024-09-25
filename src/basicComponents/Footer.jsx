e0eb10f13574857f2f2066a940529cf5ce135206
mücahit tekbaş <tekbasmucahit6@gmail.com>
2024-09-25 22:29:15 +0300

    updated

--- src/basicComponents/Footer.jsx
+++ src/basicComponents/Footer.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import Logo from './Logo'
-
+import { FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <div className='bg-blue-900 p-5'>
         <div className='grid grid-rows xl:grid-cols-3 xl:grid-rows-none lg:grid-cols-3 lg:grid-rows-none md:grid-cols-2 md:grid-rows-none gap-10'>
           
-          <div className='flex flex-col text-white justify-center items-start gap-3'>
-           <strong className='border-b-2 border-white pb-2 text-xl'>CONTACT DETAİLS</strong>
-           <p>yeniyurt mahallesi gürcan özbek caddesi 3.sokak no 12 tokat sanayi sitesi</p>
-           <p>Merkez / Tokat</p>
-           <p><a href="tel:05456191982">+90(545) 619 1982</a></p>
+          <div className='grid grid-rows text-white gap-3'>
+            <strong className='border-b-2 border-white pb-2 text-xl'>CONTACT DETAİLS</strong>
+            <p>yeniyurt mahallesi gürcan özbek caddesi 3.sokak no 12 tokat sanayi sitesi</p>
+            <p>Merkez / Tokat</p>
+            <p><a href="tel:05456191982">+90(545) 619 1982</a></p>
           </div>
 
-          <div className='flex flex-col text-white justify-center items-start'>
+          <div className='grid grid-rows text-white gap-3'>
+            <strong className='border-b-2 border-white pb-2 text-xl'>SOCİAL MEDİA</strong>
+            <div className='text-3xl text-white hover:cursor-pointer hover:text-pink-600'>
+                <FaInstagram />
+            </div>
           </div>
+
+          <div className='grid grid-rows text-white gap-3'>
+            <strong className='border-b-2 border-white pb-2 text-xl'>BRAND İDENTİTY</strong>
+            <Logo />
+          </div>
+
 
 
 
--- src/components/Firmas.jsx
+++ src/components/Firmas.jsx
@@ -4,14 +4,14 @@ function Firmas() {
   return (
     <div className='flex flex-col justify-center items-center gap-10 mt-16 mb-16'>
         <h1>TEDARİKÇİ FİRMALAR</h1>
-        <div className='flex flex-col xl:flex-row lg:flex-row justify-center items-center gap-14'>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Adopen</i>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Alfapen</i>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Adokapı</i>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Adoplaswin</i>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Penwood</i>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Accado</i>
-            <i className='bg-amber-500 p-7 text-white border rounded hover:scale-110 transition-all'>Geviss</i>
+        <div className='grid grid-cols-2 xl:grid-cols-3 lg:grid-cols-3 gap-14'>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border hover:scale-110 transition-all'>Adopen</i>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border rounded hover:scale-110 transition-all'>Alfapen</i>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border rounded hover:scale-110 transition-all'>Adokapı</i>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border rounded hover:scale-110 transition-all'>Adoplaswin</i>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border rounded hover:scale-110 transition-all'>Penwood</i>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border rounded hover:scale-110 transition-all'>Accado</i>
+            <i className='bg-blue-700 hover:bg-amber-500 p-5 text-center cursor-pointer text-white border rounded hover:scale-110 transition-all'>Geviss</i>
         </div>
     </div>
   )
