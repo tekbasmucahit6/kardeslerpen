51356e6d55fa973e518e159f35cefd8a68414a0a
mücahit tekbaş <tekbasmucahit6@gmail.com>
2024-09-26 00:02:34 +0300

    added

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -5,22 +5,22 @@ function Photos() {
     <div className='flex flex-col gap-10 mt-10 mb-10 justify-center items-center'>
       <h1>FOTOĞRAF RULOSU</h1>
       <div className='grid grid-cols-2 xl:grid-cols-4 xl:grid-rows-none lg:grid-cols-3 lg:grid-rows-none md:grid-cols-3 md:grid-rows-none gap-16'>
-        <img src="/public/img/resim1.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim2.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim3.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim4.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim5.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim6.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim7.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim8.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim9.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim10.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim11.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim12.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim13.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim14.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim15.jpg" className='w-64 rounded-xl' alt="" />
-        <img src="/public/img/resim16.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim1.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim2.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim3.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim4.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim5.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim6.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim7.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim8.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim9.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim10.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim11.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim12.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim13.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim14.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim15.jpg" className='w-64 rounded-xl' alt="" />
+        <img src="../img/resim16.jpg" className='w-64 rounded-xl' alt="" />
       </div>
     </div>
   )
